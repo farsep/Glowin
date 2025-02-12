52d5ea56f26b759d1b38e6ebfd664a5154458de3
Farid <faridespinoza05@hotmail.com>
2025-02-12 18:48:15 -0500

    Project Started

---
+++ src/main/java/com/glowin/GlowinApplication.java
@@ -0,0 +1,13 @@
+package com.glowin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GlowinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GlowinApplication.class, args);
+	}
+
+}
