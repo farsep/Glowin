e68ffbd95a5a6de2f52affa267a0168d7283cadf
Farid <faridespinoza05@hotmail.com>
2025-03-06 13:39:19 -0500

    possibly solution for the pipeline env vars

--- src/main/java/com/glowin/service/EmailService.java
+++ src/main/java/com/glowin/service/EmailService.java
@@ -15,6 +15,38 @@ public class EmailService {
     @Value("${SPRING_MAIL_USERNAME}")
     private String mailUsername;
 
+    @Value("${SPRING_MAIL_HOST}")
+    private String mailHost;
+
+    @Value("${SPRING_MAIL_PORT}")
+    private String mailPort;
+
+    @Value("${SPRING_MAIL_PASSWORD}")
+    private String mailPassword;
+
+    @Value("${SPRING_MAIL_PROPERTIES_MAIL_SMTP_AUTH}")
+    private String mailSmtpAuth;
+
+    @Value("${SPRING_MAIL_PROPERTIES_MAIL_SMTP_STARTTLS_ENABLE}")
+    private String mailStarttlsEnable;
+
+    @Value("${SPRING_MAIL_PROPERTIES_MAIL_SMTP_SSL_TRUST}")
+    private String mailSslTrust;
+
+    @Value("${SPRING_MAIL_PROPERTIES_MAIL_SMTP_LOCALHOST}")
+    private String mailLocalhost;
+
+    public void logMailProperties() {
+        System.out.println("SPRING_MAIL_USERNAME: " + mailUsername);
+        System.out.println("SPRING_MAIL_HOST: " + mailHost);
+        System.out.println("SPRING_MAIL_PORT: " + mailPort);
+        System.out.println("SPRING_MAIL_PASSWORD: " + mailPassword);
+        System.out.println("SPRING_MAIL_PROPERTIES_MAIL_SMTP_AUTH: " + mailSmtpAuth);
+        System.out.println("SPRING_MAIL_PROPERTIES_MAIL_SMTP_STARTTLS_ENABLE: " + mailStarttlsEnable);
+        System.out.println("SPRING_MAIL_PROPERTIES_MAIL_SMTP_SSL_TRUST: " + mailSslTrust);
+        System.out.println("SPRING_MAIL_PROPERTIES_MAIL_SMTP_LOCALHOST: " + mailLocalhost);
+    }
+
     public void sendConfirmationEmail(String to, String subject, String text) {
         SimpleMailMessage message = new SimpleMailMessage();
         message.setTo(to);
