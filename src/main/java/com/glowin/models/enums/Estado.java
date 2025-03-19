228ca32a3d3ecb7a585e5ba66e0a599d9f8bf719
Farid <faridespinoza05@hotmail.com>
2025-03-18 20:34:33 -0500

    mend

--- src/main/java/com/glowin/models/enums/Estado.java
+++ src/main/java/com/glowin/models/enums/Estado.java
@@ -1,13 +1,23 @@
 package com.glowin.models.enums;
 
 public enum Estado {
-    CONCLUIDA,
-    EN_CURSO,
-    CONFIRMADA,
-    CANCELADA;
+    CONCLUIDA("CONCLUIDA"),
+    EN_CURSO("EN CURSO"),
+    CONFIRMADA("CONFIRMADA"),
+    CANCELADA("CANCELADA");
+
+    private final String fullName;
+
+    Estado(String s) {
+        fullName = s;
+    }
 
     public static Estado fromString(String estado) {
         //now created a constructor that doesn't distingish between upper and lower case or even spaces
-        return Estado.valueOf(estado.toUpperCase().trim().replace(" ", ""));
+        return Estado.valueOf(estado.toUpperCase().trim().replace(" ", "_"));
+    }
+
+    public String fullName() {
+        return fullName;
     }
 }
