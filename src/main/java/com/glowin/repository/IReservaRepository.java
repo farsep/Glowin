4b3fc4b79485526d92422c2b29fd771398d63b4f
Farid <faridespinoza05@hotmail.com>
2025-03-19 15:14:12 -0500

    Retorno de Slots disponibles por rango de fechas

--- src/main/java/com/glowin/repository/IReservaRepository.java
+++ src/main/java/com/glowin/repository/IReservaRepository.java
@@ -5,7 +5,9 @@ import com.glowin.models.Reserva;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDate;
 import java.util.List;
 
 public interface IReservaRepository extends JpaRepository<Reserva, Long> {
@@ -15,10 +17,9 @@ public interface IReservaRepository extends JpaRepository<Reserva, Long> {
 
     Page<Reserva> findByServicioId(Long id, Pageable pageable);
 
-    Page<Reserva> findByFechaBetween(String fechaInicio, String fechaFin, Pageable pageable);
+    Page<Reserva> findByFechaBetween(LocalDate fecha, LocalDate fecha2, Pageable pageable);
 
     Page<Reserva> findByClienteId(Long id, Pageable pageable);
 
-    /*@Query("SELECT r FROM Reserva r WHERE r.servicio.id = :idServicio AND r.fecha BETWEEN :fechaInicio AND :fechaFin AND r.estado = 'PENDIENTE'")
-    Page<Reserva> findAvailableReservas(Long idServicio, String fechaInicio, String fechaFin);*/
+    Page<Reserva> findByFechaBetweenAndServicioId(LocalDate parse, LocalDate parse1, Long idServicio, Pageable pageable);
 }
