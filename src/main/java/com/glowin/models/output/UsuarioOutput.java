2467bee92e6e98e736fd9fc773b2e95d0f1151d1
andavidp95 <andavidp95@gmail.com>
2025-03-31 08:17:12 -0500

    email y celular en usuario output

--- src/main/java/com/glowin/models/output/UsuarioOutput.java
+++ src/main/java/com/glowin/models/output/UsuarioOutput.java
@@ -3,8 +3,8 @@ package com.glowin.models.output;
 import com.glowin.models.Usuario;
 import com.glowin.models.enums.Rol;
 
-public record UsuarioOutput(Long id, String nombre, String apellido, Rol rol) {
+public record UsuarioOutput(Long id, String nombre, String apellido, Rol rol, String email, String celular) {
     public UsuarioOutput(Usuario usuario) {
-        this(usuario.getId(), usuario.getNombre(), usuario.getApellido(), usuario.getRol());
+        this(usuario.getId(), usuario.getNombre(), usuario.getApellido(), usuario.getRol(), usuario.getEmail(), usuario.getCelular());
     }
 }
