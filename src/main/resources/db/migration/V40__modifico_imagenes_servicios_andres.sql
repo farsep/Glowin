8cd81b8ad082da9a4938d6b18d14ac3eff3bb8d1
andavidp95 <andavidp95@gmail.com>
2025-04-01 15:13:47 -0500

    nuevas imagenes

---
+++ src/main/resources/db/migration/V40__modifico_imagenes_servicios_andres.sql
@@ -0,0 +1,105 @@
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3RlbccG.jpg'
+WHERE id = 170;
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3Rlb194.md.jpg'
+WHERE id = 171;
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3RlDCp2.jpg'
+WHERE id = 172;
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3RlbGN2.md.jpg'
+WHERE id = 173;
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3RlbEAl.md.jpg'
+WHERE id = 174;
+
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3RlbMtS.jpg'
+WHERE id = 175;
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3RlbWo7.jpg'
+WHERE id = 176;
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3RlbXV9.md.jpg'
+WHERE id = 177;
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3RlbhPe.md.jpg'
+WHERE id = 178;
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3RlbwKu.jpg'
+WHERE id = 179;
+
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3RlbNcb.jpg'
+WHERE id = 180;
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3RlbOSj.md.jpg'
+WHERE id = 181;
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3RlbkHx.md.jpg'
+WHERE id = 182;
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3RlbvAQ.md.jpg'
+WHERE id = 183;
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3Rlb8NV.md.jpg'
+WHERE id = 184;
+
+
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3RlbSDB.jpg'
+WHERE id = 185;
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3RlbgoP.md.jpg'
+WHERE id = 186;
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3RlbrV1.md.jpg'
+WHERE id = 187;
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3Rlb4iF.jpg'
+WHERE id = 188;
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3Rlbila.md.jpg'
+WHERE id = 189;
+
+
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3RlbPKg.jpg'
+WHERE id = 190;
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3RlbsUJ.md.jpg'
+WHERE id = 191;
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3RlbQHv.md.jpg'
+WHERE id = 192;
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://freeimage.host/i/3RlbZRR'
+WHERE id = 193;
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3RlbtNp.md.jpg'
+WHERE id = 194;
