5e0d1b5e9af69ae1e180c87fa062fa870ee83cde
andavidp95 <andavidp95@gmail.com>
2025-04-02 11:19:55 -0500

    nuevas imagenes y DER actualizado. Ultimo commit del proyecto, muchas gracias

---
+++ src/main/resources/db/migration/V46__modifico_imagenes_servicios_andres.sql
@@ -0,0 +1,86 @@
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3RmQfuj.jpg'
+WHERE id = 193;
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3RmVCUg.jpg'
+WHERE id = 160;
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3RmVx5J.jpg'
+WHERE id = 161;
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3RmVzOv.jpg'
+WHERE id = 162;
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3RmVoJa.jpg'
+WHERE id = 163;
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3RmVAWN.jpg'
+WHERE id = 164;
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3RmVAWN.jpg'
+WHERE id = 165;
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3RmVY0X.jpg'
+WHERE id = 166;
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3RmVagn.jpg'
+WHERE id = 167;
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3RmVlJs.jpg'
+WHERE id = 168;
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3RmV05G.jpg'
+WHERE id = 169;
+
+
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3RmVEb4.jpg'
+WHERE id = 245;
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3RmVEb4.jpg'
+WHERE id = 246;
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3RmVMzl.jpg'
+WHERE id = 247;
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3RmVVX2.md.jpg'
+WHERE id = 248;
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3RmVWsS.md.jpg'
+WHERE id = 249;
+
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3RmVhq7.jpg'
+WHERE id = 250;
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3RmVj19.md.jpg'
+WHERE id = 251;
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3RmVwge.md.jpg'
+WHERE id = 252;
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3RmVOdu.md.jpg'
+WHERE id = 253;
+
+UPDATE imagenes_servicios
+SET url_imagen = 'https://iili.io/3RmVe5b.md.jpg'
+WHERE id = 254;
